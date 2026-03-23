d28d65fbf6f539215898b45dae34d0654ec46024
solreven <blomlie@proton.me>
2026-03-24 00:37:29 +0100

    v.0.02 - Adds tasks to an empty list

--- src/task.ts
+++ src/task.ts
@@ -2,7 +2,6 @@ export const createTask = (
   title: string,
   description: string,
   priority: string,
-  status: string,
 ) => {
   return {
     // Claude says there's a shorthand for when you know the key and value are the same
@@ -10,6 +9,10 @@ export const createTask = (
     title: title,
     description: description,
     priority: priority,
-    status: status,
+    status: "todo",
   };
+};
+// I need to deal with these any-types so that it actually makes sense to use TS
+export const addTask = (list: any, task: any) => {
+  return list.concat(task);
 };
--- src/tests/task.test.ts
+++ src/tests/task.test.ts
@@ -1,7 +1,7 @@
-import { createTask } from "../task";
+import { createTask, addTask } from "../task";
 describe("Task creation", () => {
   it("should create a new task with title, description, priority, status and an id", () => {
-    const task = createTask("Task 1", "Description 1", "high", "todo");
+    const task = createTask("Task 1", "Description 1", "high");
     expect(task).toEqual({
       id: expect.any(String),
       title: "Task 1",
@@ -9,5 +9,15 @@ describe("Task creation", () => {
       priority: "high",
       status: "todo",
     });
+  });
+});
+describe("adding tasks", () => {
+  it("adds a task to an empty list and the list is equal to the task", () => {
+    const list = [];
+    const task = createTask("Task 1", "Description 1", "high");
+
+    const result = addTask(list, task);
+
+    expect(result).toHaveLength(1);
   });
 });
